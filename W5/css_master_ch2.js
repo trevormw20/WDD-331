57e7d811ebe6070d122eb405e3b182daf777d58f
craigkehl <keh19003@byui.edu>
2020-05-19 22:15:01 -0600

    w5

---
+++ W5/css_master_ch2.js
@@ -0,0 +1,7 @@
+var move = document.getElementById('move');
+move.addEventListener('click', function(e) {
+    var objs = document.querySelectorAll('div');
+    objs.forEach((o) => {
+        o.classList.toggle('moved');
+    });
+});
